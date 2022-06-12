8d454aea19a0d545c7304269f8dd627ce444a0a4
Volha Koval <rijaiaola@gmail.com>
2022-06-13 02:16:04 +0300

    feat: add 3 module task 2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newArray = [];
+  for (let element of arr) {
+    if (element >= a && element <= b) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 }
