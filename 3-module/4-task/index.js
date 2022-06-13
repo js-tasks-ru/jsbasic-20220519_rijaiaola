fa13bf88b96e7572b6d4e49c0fc868130fc39ae3
Volha Koval <rijaiaola@gmail.com>
2022-06-13 03:35:36 +0300

    feat: add 3 module task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = [];
+
+  for (let user of users) {
+    if (user.age <= age) {
+      result.push(`${user.name}, ${user.balance}`);
+      result.push('\n');
+    }
+  }
+  result.pop();
+
+  return result.join('');
 }
