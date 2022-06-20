d83897ef5594e5e9cd03276c92de142cdb85c222
Volha Koval <rijaiaola@gmail.com>
2022-06-21 01:06:03 +0300

    feat: add 5 module task 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const carousel = document.querySelector('.carousel__inner');
+  const slide = document.querySelector('.carousel__slide');
+  const slideCount = document.getElementsByClassName('carousel__slide').length;
+  const moveValue = slide.offsetWidth;
+  let currentMove = 0;
+  let currentClick = 0;
+  const maxCount = slideCount*moveValue;
+
+  arrowRight.style.display = '';
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () => {
+
+      arrowRight.style.display = '';
+      currentMove += moveValue;
+      currentClick ++;
+      carousel.style.transform = `translateX(-${currentMove}px)`;
+      arrowLeft.style.display = '';
+
+    if (currentClick === 3) {
+      arrowRight.style.display = 'none';
+    }
+  });
+
+  arrowLeft.addEventListener('click', () => {
+
+      arrowLeft.style.display = '';
+      currentMove -= moveValue;
+      currentClick --;
+      carousel.style.transform = `translateX(-${currentMove}px)`;
+      arrowRight.style.display = '';
+
+      (currentClick === 0) ? arrowLeft.style.display = 'none' : arrowLeft.style.display = '';
+    })
+
 }
