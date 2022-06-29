1ed4e468967c8c14b3b267cb679ef3658bb1dfcf
Volha Koval <rijaiaola@gmail.com>
2022-06-30 01:20:28 +0300

    feat: [wip] add  6 module tasks 1,2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,3 +1,5 @@
+import createElement from '../../assets/lib/create-element.js';
+
 /**
  * Компонент, который реализует таблицу
  * с возможностью удаления строк
@@ -13,6 +15,66 @@
  *
  */
 export default class UserTable {
+  #items = [];
+  #parent;
+  // #container;
+  // #cont;
+  elem;
+
   constructor(rows) {
+    this.#items = rows;
+    // this.#parent = document.createElement('div');
+    this.#parent = document.createElement('table');
+    // this.#container = this.#parent.innerHTML;
+    // this.#cont = this.#template();
+    // this.elem = this.#cont;
+    this.elem = createElement(this.render());
+
+    // console.log(this.#parent.innerHTML);
+    // this.render();
+  }
+
+  get container() {
+    return this.#parent.querySelectorAll('button');
+  }
+
+  // #onQuitButton () => {
+
+  // }
+
+  #template() {
+    return `
+          <table>
+            <thead>
+                <tr>
+                    <th>Имя</th>
+                    <th>Возраст</th>
+                    <th>Зарплата</th>
+                    <th>Город</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                ${this.#items.map(item => `<tr>
+                                              <td>${item.name}</td>
+                                              <td>${item.age}</td>
+                                              <td>${item.salary}
+                                              </td><td>${item.city}</td>
+                                              <td><button>X</button></td>
+                                            </tr>` )
+                            .join('')}
+            </tbody>
+          </table>
+        `
+
+  }
+
+   render() {
+    return this.#parent.innerHTML = this.#template();
+
+    // this.#parent.innerHTML = createElement(this.#template());
+    // let a = this.#parent.innerHTML;
+    //   a = this.#template();
+    // return a;
   }
 }
